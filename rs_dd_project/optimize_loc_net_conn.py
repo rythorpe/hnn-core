655b24decdce1043864c271d038ec17f294bb69f
rythorpe <ryan_thorpe@brown.edu>
2023-02-22 17:05:29 -0500

    rename project folder

--- rs_dd_project/optimize_loc_net_conn.py
+++ rs_dd_project/optimize_loc_net_conn.py
@@ -17,3 +17,10 @@ from hnn_core.network_models import L6_model, calcium_model
 from hnn_core.viz import plot_dipole
 
 hnn_core_root = op.join(op.dirname(hnn_core.__file__))
+
+
+def get_conn_params(connection):
+    return connection['nc_dict']['A_weight'], connection['nc_dict']['lamtha']
+
+
+net = L6_model(connect_layer_6=True, legacy_mode=False)
