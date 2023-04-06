0a334acdd4d1e3be0642bfad1e21d95bcaab6395
Ryan Thorpe <ryvthorpe@gmail.com>
2023-04-06 18:01:07 -0400

    fix bug in csv formatting

--- rs_dd_project/opt_baseline_drive.py
+++ rs_dd_project/opt_baseline_drive.py
@@ -107,7 +107,7 @@ opt_params[:-1] = [10 ** weight for weight in opt_params[:-1]]
 header = [weight + '_weight' for weight in poiss_weights_0] + ['poiss_rate']
 header = ','.join(header)
 np.savetxt(op.join(output_dir, 'optimized_baseline_drive_params.csv'),
-           X=np.c_[opt_params], delimiter=',', header=header)
+           X=[opt_params], delimiter=',', header=header)
 print(f'poiss_weights: {opt_params[:-1]}')
 print(f'poiss_rate: {opt_params[-1]}')
 
