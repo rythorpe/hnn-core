3d7bb09d6e5fc7aeb102cbcd440201e7623f28c2
Ryan Thorpe <ryvthorpe@gmail.com>
2023-04-06 15:49:40 -0400

    fix header append in opt_baseline_drive.py

--- rs_dd_project/opt_baseline_drive.py
+++ rs_dd_project/opt_baseline_drive.py
@@ -104,7 +104,7 @@ opt_results = gp_minimize(func=opt_min_func,
 opt_params = opt_results.x
 # convert param back from log_10 scale
 opt_params[:-1] = [10 ** weight for weight in opt_params[:-1]]
-header = [weight + '_weight' for weight in poiss_weights_0] + 'poiss_rate'
+header = [weight + '_weight' for weight in poiss_weights_0] + ['poiss_rate']
 np.savetxt(op.join(output_dir, 'optimized_baseline_drive_params.csv'),
            X=opt_params, delimiter=',', header=header)
 print(f'poiss_weights: {[10 ** param for param in opt_params[:-1]]}')
