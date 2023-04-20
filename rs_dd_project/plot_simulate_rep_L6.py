1882fabb04c6d0113b11dabdec6cc3cabfdb80ab
rythorpe <ryan_thorpe@brown.edu>
2023-04-19 23:50:48 -0400

    adjust main repetition/dev plot

--- rs_dd_project/plot_simulate_rep_L6.py
+++ rs_dd_project/plot_simulate_rep_L6.py
@@ -8,15 +8,15 @@ from urllib.request import urlretrieve
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-###############################################################################
-# Let us import hnn_core
+plt.ion()
 
 import hnn_core
 from hnn_core import simulate_dipole, MPIBackend, JoblibBackend, read_dipole
-from hnn_core.network_models import L6_model, calcium_model
+from hnn_core.network_models import L6_model
 from hnn_core.viz import plot_dipole
 
+###############################################################################
+# Read in empirical data to compare to simulated data
 data_url = ('https://raw.githubusercontent.com/jonescompneurolab/hnn/master/'
             'data/MEG_detection_data/S1_SupraT.txt')
 #urlretrieve(data_url, 'S1_SupraT.txt')
@@ -33,6 +33,9 @@ rep_interval = 100.  # in ms; 10 Hz
 rep_duration = 100.  # 170 ms for human M/EEG
 
 syn_depletion_factor = 0.8  # used to simulate successive synaptic depression
+
+t_prox = 34.  # time (ms) of the proximal drive relative to stimulus rep
+t_dist = 65.  # time (ms) of the distal drive relative to stimulus rep
 
 event_seed = 1
 conn_seed = 1
@@ -70,7 +73,7 @@ for rep_idx, rep_time in enumerate(rep_start_times):
         depression_factor = 1
     else:
         # attenuate syn weight values as a function of # of reps
-        depression_factor = syn_depletion_factor ** (rep_idx + 1)
+        depression_factor = syn_depletion_factor ** rep_idx
     weights_ampa_prox_depr = {key: val * depression_factor
                               for key, val in weights_ampa_prox.items()}
     weights_ampa_L6_depr = {key: val * depression_factor
@@ -81,13 +84,13 @@ for rep_idx, rep_time in enumerate(rep_start_times):
                             'L5_basket': 1., 'L5_pyramidal': 1.}
     # note that all NMDA weights are zero
     net.add_evoked_drive(
-        f'evprox_rep{rep_idx}', mu=rep_time + 34., sigma=2.47, numspikes=1,
+        f'evprox_rep{rep_idx}', mu=rep_time + t_prox, sigma=2.47, numspikes=1,
         weights_ampa=weights_ampa_prox_depr, weights_nmda=None,
         location='proximal', synaptic_delays=synaptic_delays_prox,
         probability=1.0, conn_seed=conn_seed, event_seed=event_seed)
     # Hack: prox_1 should also target the distal projections of L6 Pyr
     net.add_evoked_drive(
-        f'evprox_rep{rep_idx}_L6', mu=rep_time + 34., sigma=2.47,
+        f'evprox_rep{rep_idx}_L6', mu=rep_time + t_prox, sigma=2.47,
         numspikes=1, weights_ampa=weights_ampa_L6_depr,
         location='distal', synaptic_delays=0.1,
         probability=0.33, conn_seed=conn_seed, event_seed=event_seed)
@@ -100,7 +103,7 @@ for rep_idx, rep_time in enumerate(rep_start_times):
     synaptic_delays_dist = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
                             'L5_pyramidal': 0.1}
     net.add_evoked_drive(
-        f'evdist_rep{rep_idx}', mu=rep_time + 65, sigma=3.85, numspikes=1,
+        f'evdist_rep{rep_idx}', mu=rep_time + t_dist, sigma=3.85, numspikes=1,
         weights_ampa=weights_ampa_dist, weights_nmda=weights_nmda_dist,
         location='distal', synaptic_delays=synaptic_delays_dist,
         probability=1.0, conn_seed=conn_seed, event_seed=event_seed)
@@ -117,17 +120,38 @@ for dpl in dpls:
 
 ###############################################################################
 # Plot the amplitudes of the simulated aggregate dipole moments over time
+gridspec = {'width_ratios': [1], 'height_ratios': [1, 3, 3]}
 fig, axes = plt.subplots(3, 1, sharex=True, figsize=(6, 6),
-                         constrained_layout=True)
-net.cell_response.plot_spikes_hist(ax=axes[0], n_bins=200,
-                                   spike_types=['evprox', 'evdist'],
-                                   show=False)
+                         gridspec_kw=gridspec, constrained_layout=True)
+#### plot arrow for each drive
+arrow_height = 1.0
+head_length = 0.2
+head_width = 10.0
+for rep_idx, rep_time in enumerate(rep_start_times):
+    if rep_idx == reps - 1:  # last rep
+        depression_factor = 1
+    else:
+        depression_factor = syn_depletion_factor ** rep_idx
+    axes[0].arrow(rep_time + t_prox, 0, 0, arrow_height * depression_factor,
+                  fc='k', ec=None,
+                  alpha=1., width=1, head_width=head_width,
+                  head_length=head_length, length_includes_head=True)
+    axes[0].arrow(rep_time + t_dist, 1, 0, -arrow_height, fc='k', ec=None,
+                  alpha=1., width=1, head_width=head_width,
+                  head_length=head_length, length_includes_head=True)
+axes[0].set_yticks([0, 1])
+axes[0].set_ylabel('drive\nstrength')
+###
 for rep_time in rep_start_times:
     axes[0].axvline(rep_time, c='k')
     axes[1].axvline(rep_time, c='k')
     axes[2].axvline(rep_time, c='w')
-plot_dipole(dpls, ax=axes[1], layer='agg', show=False)
-emp_dpl.plot(ax=axes[1], color='tab:purple', show=False)
+net.cell_response.plot_spikes_hist(ax=axes[1], n_bins=200,
+                                   spike_types=['L2_basket', 'L2_pyramidal',
+                                                'L5_basket', 'L5_pyramidal',
+                                                'L6_basket', 'L6_pyramidal'],
+                                   show=False)
 net.cell_response.plot_spikes_raster(ax=axes[2], show=False)
+axes[2].set_xlim([0, tstop])
 plot_dipole(dpls, average=False, layer=['L2', 'L5', 'L6', 'agg'], show=False)
 plt.show()
