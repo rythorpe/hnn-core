3bafab3f79adb9a4edcad9aa1cbd9e7f7cc2fed6
Ryan Thorpe <ryvthorpe@gmail.com>
2023-04-20 15:52:30 -0400

    opt worked! now, refine optimized baseline drive params with manual tuning

--- rs_dd_project/optimization.py
+++ rs_dd_project/optimization.py
@@ -35,9 +35,9 @@ def set_conn_params(net, conn_params):
 def plot_net_response(dpls, net, sim_time):
     fig, axes = plt.subplots(6, 1, sharex=True, figsize=(6, 12))
 
-    #window_len, scaling_factor = 30, 2000
-    #for dpl in dpls:
-    #    dpl.smooth(window_len).scale(scaling_factor)
+    # window_len, scaling_factor = 30, 2000
+    # for dpl in dpls:
+    #     dpl.smooth(window_len).scale(scaling_factor)
 
     net.cell_response.plot_spikes_hist(ax=axes[0], n_bins=sim_time, show=False)
     plot_dipole(dpls, ax=axes[1:5], layer=['L2', 'L5', 'L6', 'agg'],
@@ -47,8 +47,8 @@ def plot_net_response(dpls, net, sim_time):
 
 
 def plot_spiking_profiles(net, sim_time, burn_in_time, target_spike_rates):
-    #custom_params = {"axes.spines.right": False, "axes.spines.top": False}
-    #sns.set_theme(style="ticks", rc=custom_params)
+    # custom_params = {"axes.spines.right": False, "axes.spines.top": False}
+    # sns.set_theme(style="ticks", rc=custom_params)
 
     fig, ax = plt.subplots(1, 1, figsize=(6, 6))
 
@@ -86,8 +86,8 @@ def plot_spiking_profiles(net, sim_time, burn_in_time, target_spike_rates):
                                'target rate': pop_targets})
     ax = sns.barplot(data=spiking_df, x='spike rate', y='layer',
                      hue='cell type', palette='Greys', errorbar='se', ax=ax)
-     # note: eyeball dodge value
-     # also, setting legend='_nolegend_' doesn't work when hue is set
+    # note: eyeball dodge value
+    # also, setting legend='_nolegend_' doesn't work when hue is set
     ax = sns.pointplot(data=spiking_df, x='target rate', y='layer',
                        hue='cell type', join=False, dodge=0.4, color='k',
                        markers='D', ax=ax)
@@ -96,6 +96,13 @@ def plot_spiking_profiles(net, sim_time, burn_in_time, target_spike_rates):
     ax.set_xlabel('mean single-unit spike rate (Hz)')
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles=handles[2:], labels=labels[2:])
+
+    # make sure calcuation above is consistent with mean_rates method
+    # avg_spike_rates = net.cell_response.mean_rates(tstart=burn_in_time,
+    #                                                tstop=sim_time,
+    #                                                gid_ranges=net.gid_ranges)
+    # print(avg_spike_rates)
+    # print(spiking_df.groupby(['layer', 'cell type'])['spike rate'].mean())
 
     return fig
 
