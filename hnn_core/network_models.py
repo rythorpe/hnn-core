b758b7afa64e4a455e499568671a2236213a8fb3
rythorpe <ryan_thorpe@brown.edu>
2023-08-09 00:14:37 -0400

    L6_model() now starts with local net conn identical to updated_calcium() model

--- hnn_core/network_models.py
+++ hnn_core/network_models.py
@@ -351,8 +351,8 @@ def L6_model(params=None, add_drives_from_params=False,
     # del net.connectivity[10]
 
     # Modify L5_basket -> L5_pyramidal inhibition
-    net.connectivity[6]['nc_dict']['A_weight'] = 0.025  # gabaa
-    net.connectivity[7]['nc_dict']['A_weight'] = 0.006  # gabab
+    # net.connectivity[6]['nc_dict']['A_weight'] = 0.025  # gabaa
+    # net.connectivity[7]['nc_dict']['A_weight'] = 0.006  # gabab
 
     delay = net.delay
 
