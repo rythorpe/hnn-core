7abcb0e4cfb6849274c7ebeeb796c3f79f079990
Ryan Thorpe <ryvthorpe@gmail.com>
2023-11-20 02:02:12 -0500

    [wip] baseline conn: retune net

--- hnn_core/network_models.py
+++ hnn_core/network_models.py
@@ -355,32 +355,32 @@ def L6_model(params=None, add_drives_from_params=False,
     #         net.cell_types[cell_type].synapses['gabab']['tau1'] = 45.0
     #         net.cell_types[cell_type].synapses['gabab']['tau2'] = 200.0
 
-    conn_weights = {"L2e_L2e_ampa": 0.00064,  # 0.00070
+    conn_weights = {"L2e_L2e_ampa": 0.00066,  # 0.00070
                     "L2e_L2e_nmda": 0.00001,
                     "L2i_L2e_gabaa": 0.0040,
                     "L2i_L2e_gabab": 0.0020,
-                    "L2e_L2i_ampa": 0.00099,  # 0.00090
+                    "L2e_L2i_ampa": 0.00108,  # 0.00090
                     "L2i_L2i_gabaa": 0.02,
-                    "L6i_cross_L2e_gabaa": 0.003,
-                    "L2e_L5e_ampa": 0.0001,
+                    "L6i_cross_L2e_gabaa": 0.05,
+                    "L2e_L5e_ampa": 0.0008,
                     "L2i_L5e_gabaa": 0.002,
-                    "L5e_L5e_ampa": 0.00078,  # 0.00077
+                    "L5e_L5e_ampa": 0.0009,  # 0.00077
                     "L5e_L5e_nmda": 0.00001,
-                    "L5i_L5e_gabaa": 0.01075,  # 0.018
+                    "L5i_L5e_gabaa": 0.01065,  # 0.018
                     "L5i_L5e_gabab": 0.00005,  # changed from jones09
                     "L6i_cross_L5e_gabaa": 0.01,
                     "L2e_L5i_ampa": 0.00010,
-                    "L5e_L5i_ampa": 0.00020,  # 0.00043
+                    "L5e_L5i_ampa": 0.00015,  # 0.00043
                     "L5i_L5i_gabaa": 0.02,
                     "L5e_L6e_ampa": 0.00002,
                     "L6e_L6e_ampa": 0.00063,
                     "L6e_L6e_nmda": 0.00001,
                     "L6i_L6e_gabaa": 0.0040,
-                    "L6i_L6e_gabab": 0.002,
+                    "L6i_L6e_gabab": 0.0020,
                     "L6e_L6i_ampa": 0.00099,
                     "L6i_L6i_gabaa": 0.02}
     lamtha = 4.0
-    lamtha_L6_cross = 4.0
+    lamtha_L6_cross = 8.0
     delay = net.delay
     conn_seed = 1  # using the same seed will enforce matching subpop conn!!!
 
@@ -394,6 +394,7 @@ def L6_model(params=None, add_drives_from_params=False,
     prob_i_i = 0.33  # 0.66
     prob_e_i = 0.67  # 0.66
     prob_i_e_cross = 0.67
+    prob_e_e_5 = 0.125
 
     # layer5 Pyr -> layer5 Pyr
     for receptor in ['nmda', 'ampa']:
@@ -405,7 +406,7 @@ def L6_model(params=None, add_drives_from_params=False,
                            delay=delay,
                            lamtha=lamtha,
                            allow_autapses=False,
-                           probability=prob_e_e,
+                           probability=prob_e_e_5,
                            conn_seed=conn_seed)
 
     # layer5 Basket -> layer5 Pyr
@@ -540,10 +541,10 @@ def L6_model(params=None, add_drives_from_params=False,
                 prob_e_i = 0.67
             else:
                 # between-group connection probabilities
-                prob_e_e = 0.01
+                prob_e_e = 0.08
                 prob_i_e = 0.90
-                prob_i_i = 0.33
-                prob_e_i = 0.10
+                prob_i_i = 0.125
+                prob_e_i = 0.08
 
             # layer2 Pyr -> layer2 Pyr
             for receptor in ['nmda', 'ampa']:
